e2590ab7f1cba683b7fbf8d7c9ccfaec66ed32ce
m1kraze <olenyak.roman@mail.ru>
2023-10-12 17:52:09 +0300

    test

--- Backend/TeamCraft/TeamCraft/Program.cs
+++ Backend/TeamCraft/TeamCraft/Program.cs
@@ -17,7 +17,21 @@ app.MapGet("/", async delegate (HttpContext context, DBConfigurator db)
     //CategoryHobby test = new CategoryHobby() {  nameHobby = "test"};
     //await db.categoryHobbies.AddAsync(test);
     //await db.SaveChangesAsync();
-    return db.categoryHobbies.ToList();
+    //return db.categoryHobbies.ToList();
+
+
+    /*Person person = new Person() { nickName = "Pon", gender = "natural", urlContact = "vfksgk" };
+    Team team = new Team() { teamName = "GAVNO", teamDescription = "fsfhketomrjg", teamGoal = "fsghrhktj" };
+    team.people = new List<Person>() { person };
+
+    db.people.Add(person);
+    db.Teams.Add(team);
+    db.SaveChanges();
+    return db.Teams.ToList();*/
+
+
+    //return db.people.ToList();
+
     //var coord = db.Coordinate.ToList();
     //return db.Users.ToList();
 });
