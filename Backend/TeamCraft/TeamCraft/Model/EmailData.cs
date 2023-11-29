2c3c9bf9c84387012bf3565d972a25f335882564
HemulenEXE <84243256+HemulenEXE@users.noreply.github.com>
2023-11-29 19:15:36 +0300

    Renew Api

---
+++ Backend/TeamCraft/TeamCraft/Model/EmailData.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamCraft.Model
+{
+    public class EmailData
+    {
+        public EmailData() { }
+        public EmailData(string mail) 
+        {
+            this.email = mail;
+        }
+
+        public string email { get; set; }
+    }
+}
--- Backend/TeamCraft/TeamCraft/Program.cs
+++ Backend/TeamCraft/TeamCraft/Program.cs
@@ -1,26 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using TeamCraft.DataBaseController;
 using TeamCraft.FilterLogic;
 using TeamCraft.Model;
 using TeamCraft.Model.TeamsArchitecture;
 using TeamCraft.Model.UserAcrhitecture;
-using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using System.Net.Mail;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// получаем строку подключения из файла конфигурации
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// добавляем контекст ApplicationContext в качестве сервиса в приложение
 builder.Services.AddDbContext<DBConfigurator>(options => options.UseSqlServer(connection));
 
-//builder.Services.AddMvc()
-//        .AddJsonOptions(
-//            options => options.JsonSerializerOptionss.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-//        );
 builder.Services.AddControllers()
        .AddJsonOptions(options =>
        {
@@ -29,26 +23,44 @@ builder.Services.AddControllers()
 
 var app = builder.Build();
 
-app.MapGet("/", async delegate (HttpContext context, DBConfigurator db)
+
+app.MapPost("/api/profile/restoration/", async delegate (HttpContext context)
 {
-    //Team teamtest = new Team("TeamName", "description", "status");
-    //db.Teams.Add(teamtest);
+    EmailData email = await context.Request.ReadFromJsonAsync<EmailData>();
+    // Отправка письма
+    // Настройки SMTP-сервера Mail.ru
+    string smtpServer = "smtp.mail.ru";
+    int smtpPort = 587;
+    string smtpUsername = "proverka_2121@mail.ru"; //твоя почта, с которой отправляется сообщение
+    string smtpPassword = "iRzNgtp4J8UKaT515daf";//пароль приложения (от почты)
 
-    //SettingsProfileUser settingsProfileUser = new SettingsProfileUser("superLogin", "coolPassword");
-    //DataUser dataUser = new DataUser("Name", "surName", "status", new DateTime(2000, 10, 10), "test");
-    //AccountUser test = new AccountUser(dataUser, settingsProfileUser);
+    // Создаем объект клиента SMTP
+    using (SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort))
+    {
+        // Настройки аутентификации
+        smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+        smtpClient.EnableSsl = true;
 
+        using (MailMessage mailMessage = new MailMessage())
+        {
+            mailMessage.From = new MailAddress(smtpUsername);
+            mailMessage.To.Add(email.email); // Укажите адрес получателя
+            mailMessage.Subject = "Заголовок сообщения (тема)";
+            mailMessage.Body = $"Текст сообщения";
 
-
-    //db.accountsUser.Add(test);
-    //MemberTeam member = new MemberTeam(dataUser);
-    //db.memberTeams.Add(member);
-
-    //teamtest.AddPersonInTeam(member);
-    var t =  db.categoryHobbies.ToList()[0].skillPeople;
-    return t;
-    //await db.SaveChangesAsync();
-    //return db.Teams.ToList();
+            try
+            {
+                // Отправляем сообщение
+                smtpClient.Send(mailMessage);
+                Console.WriteLine("Сообщение успешно отправлено.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка отправки сообщения: {ex.Message}");
+            }
+        }
+    }
 });
+
 
 app.Run();
