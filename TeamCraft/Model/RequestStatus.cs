742cfed668d03e40cc7c60ff97ecb9176312073e
leikocits <kupreev.k.161@gmail.com>
2024-03-25 22:55:04 +0300

    email and work with db

---
+++ TeamCraft/Model/RequestStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace TeamCraft.Model
+{
+    public class RequestStatus
+    {
+        public RequestStatus() 
+        {
+            this.statusCode = 200;
+            this.statusRequest = "Ok";
+            message = new List<string> { };
+        }
+        //public RequestStatus(List<string> message,int numCode = 200, string status = "OK")
+        //{
+        //    this.statusCode = numCode;
+        //    this.message = message;
+        //    this.statusRequest = status;
+        //}
+
+        public void SetBadStatus()
+        {
+            statusCode = 400;
+            this.statusRequest = "error";
+        }
+
+        public int statusCode;
+        public string statusRequest;
+        public List<string> message;
+    }
+}
