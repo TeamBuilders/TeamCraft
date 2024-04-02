fadd8d70c075aa92c943d090ec73760b92b0ee0a
Antonenko Aleksandr <114353007+daates@users.noreply.github.com>
2024-04-02 17:39:11 +0300

    added animation on home

--- Frontend/team-craft/src/pages/Home/Home.js
+++ Frontend/team-craft/src/pages/Home/Home.js
@@ -1,16 +1,49 @@
-import React from "react";
-import styles from "./Home.module.css";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import {React, useRef, useEffect, useState} from 'react';
+import styles from './Home.module.css';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 import { Link } from "react-router-dom";
 function Home() {
+  const targetRefs = useRef([]); // Массив для хранения ссылок на целевые элементы
+  const [visibilityStates, setVisibilityStates] = useState([]);
+
+  useEffect(() => {
+    const observers = []; // Массив для хранения экземпляров IntersectionObserver
+
+    // Создание нового экземпляра IntersectionObserver для каждого целевого элемента
+    targetRefs.current.forEach((targetRef, index) => {
+        if (targetRef) { // Проверяем, что ref не равен undefined
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                  setVisibilityStates(prevStates => {
+                    const newStates = [...prevStates];
+                    newStates[index] = entry.isIntersecting;
+                    console.log(newStates);
+                    return newStates;
+                  });
+                });
+            });
+
+            // Начало отслеживания изменений видимости для текущего целевого элемента
+            observer.observe(targetRef);
+
+            observers.push(observer); // Добавление экземпляра IntersectionObserver в массив
+        }
+    });
+
+    // Очистка всех экземпляров IntersectionObserver при размонтировании компонента
+    return () => {
+        observers.forEach(observer => observer.disconnect());
+    };
+}, [targetRefs]);
+
   return (
     <div className={styles.main_body}>
       <Header />
         <div className={styles.main}>
           <div className={styles.container1}>
-            <div className={styles.main_text}>
+            <div ref={ref => targetRefs.current[0] = ref} className={`${styles.main_text} ${visibilityStates[0] ? styles.visible : ''}`}>
               <p className={styles.first_text}>
                 Добро пожаловать на{" "}
                 <span className={styles.name}>TeamCraft</span> <br />
@@ -25,9 +58,9 @@ function Home() {
           </div>
         </div>
         <div className={styles.container2}>
-          <div className={styles.img_cont2}></div>
-          <div className={styles.text_cont2}>
-            <p>
+          <div  ref={ref => targetRefs.current[1] = ref} className={`${styles.img_cont2} ${visibilityStates[1] ? styles.visible : ''}`}></div>
+          <div ref={ref => targetRefs.current[2] = ref} className={`${styles.text_cont2} ${visibilityStates[2] ? styles.visible : ''}`}>
+            <p> 
               <span className={styles.cont2_head}>Найдите команду</span> <br />{" "}
               <br /> Если вы ищете команду для участия в соревнованиях, проектах
               или просто хотите расширить свой круг общения, у нас есть
@@ -36,7 +69,7 @@ function Home() {
           </div>
         </div>
         <div className={styles.container3}>
-          <div className={styles.text_cont3}>
+          <div ref={ref => targetRefs.current[3] = ref} className={`${styles.text_cont3} ${visibilityStates[3] ? styles.visible : ''}`}>
             <p>
               <span className={styles.cont3_head}>Создайте команду</span> <br />{" "}
               <br /> Если у вас уже есть идея или проект и вам нужны партнеры,
@@ -44,7 +77,7 @@ function Home() {
               разделяют ваши интересы и идеи.
             </p>
           </div>
-          <div className={styles.img_cont3}></div>
+          <div ref={ref => targetRefs.current[4] = ref} className={`${styles.img_cont3} ${visibilityStates[4] ? styles.visible : ''}`}></div>
         </div>
       <Footer />
     </div>
