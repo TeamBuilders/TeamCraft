b0b348ca4d2bbc3e75fc7e19ca34942017dca792
HemulenEXE <84243256+HemulenEXE@users.noreply.github.com>
2024-04-29 19:22:58 +0300

    Update SkillPerson.cs

--- Backend/TeamCraft/TeamCraft/FilterLogic/SkillPerson.cs
+++ Backend/TeamCraft/TeamCraft/FilterLogic/SkillPerson.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
+using TeamCraft.Model;
 
 namespace TeamCraft.FilterLogic
 {
@@ -14,7 +15,12 @@ namespace TeamCraft.FilterLogic
             this.nameSkill = name;
             this.categoryHobby = hobbyAttachment;
         }
-
+        public SkillPerson(int id, string name, CategoryHobby hobbyAttachment, int hobbyid)
+        {
+            this.id = id;
+            this.nameSkill = name;
+            this.categoryHobbyId = hobbyid;
+        }
 
         public int id { get; set; }
         public string nameSkill { get; set; }
@@ -22,40 +28,15 @@ namespace TeamCraft.FilterLogic
         [JsonIgnore]
         [Newtonsoft.Json.JsonIgnore]
         public CategoryHobby? categoryHobby { get; set; }
-        public string field1 { get; set; }
-        public string field2 { get; set; }
-        public string field3 { get; set; }
-        public string field4 { get; set; }
-        public string field5 { get; set; }
-        public string field6 { get; set; }
-        public string field7 { get; set; }
-        public string field8 { get; set; }
 
         //public int? CategoryHobbyId { get; set; }
         //public CategoryHobby? CategoryHobby { get; set; }
 
+        public override bool Equals(object? obj)
+        {
+            if (obj is SkillPerson person) return nameSkill == person.nameSkill;
+            return false;
+        }
+
     }
-
-    public class SkillPersonGameDev: SkillPerson
-    {
-        public SkillPersonGameDev()
-        {
-
-        }
-
-        public SkillPersonGameDev(string name, CategoryHobby hobbyAttachment)
-        {
-            this.nameSkill = name;
-            this.categoryHobby = hobbyAttachment;
-        }
-
-        public new string gamedev
-        {
-            get { return base.field1; }
-            set { base.field1 = value; }
-        }
-    }
-
-
-
 }
