b5d8151fcbf681b52e0a4b7cc211db6c1c533dee
HemulenEXE <84243256+HemulenEXE@users.noreply.github.com>
2024-04-29 19:45:22 +0300

    установка пакетов

--- Backend/TeamCraft/TeamCraft/FilterLogic/CategoryHobby.cs
+++ Backend/TeamCraft/TeamCraft/FilterLogic/CategoryHobby.cs
@@ -7,6 +7,12 @@
         {
             this.nameHobby = name;
         }
+
+        public CategoryHobby(int id,string name)
+        {
+            this.id = id;
+            this.nameHobby = name;
+        }
         public int id { get; set; }
         public string nameHobby { get; set; }
 
