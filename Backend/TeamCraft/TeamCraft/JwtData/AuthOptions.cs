a78597fcb4fb525fc032abeb3b4db598ba3e85e1
HemulenEXE <84243256+HemulenEXE@users.noreply.github.com>
2024-05-18 00:33:29 +0300

    добавление апи на выход из команды, рефакторинг v2

--- Backend/TeamCraft/TeamCraft/JwtData/AuthOptions.cs
+++ Backend/TeamCraft/TeamCraft/JwtData/AuthOptions.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
-namespace TeamCraft.JwtData
+public class AuthOptions
 {
-    public class AuthOptions
-    {
-        public const string ISSUER = "MyAuthServer"; // издатель токена
-        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
-        const string KEY = "Secret phase2282Secret phase2282";   // ключ для шифрации
-        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
-    }
+    public const string ISSUER = "MyAuthServer";
+    public const string AUDIENCE = "MyAuthClient";
+    static string KEY = File.ReadAllText("key.txt");
+
+
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
 }
+
